d06ffcc8f04c2999a8ca010d86bdf3d3a4e6e421
Jarmila Kaska <jarmila.kaska@gmail.com>
2024-11-30 14:51:39 +0100

    Set up bootstrapper, add Edit button

--- ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -8,6 +8,10 @@ namespace ToDoList.Frontend.Clients
 {
     public interface IToDoItemsClient
     {
+        public Task CreateItemAsync(ToDoItemView itemView);
+        public Task<ToDoItemView?> ReadItemByIdAsync(int toDoItemId);
         public Task<List<ToDoItemView>> ReadItemsAsync();
+        public Task DeleteItemAsync(ToDoItemView itemView);
+        public Task UpdateItemAsync(ToDoItemView itemView);
     }
 }
